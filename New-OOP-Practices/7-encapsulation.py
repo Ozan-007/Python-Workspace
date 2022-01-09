4681822730711a39e2e1a39078d4605e07076bc3
Ozan <ozan28061@gmail.com>
2022-01-09 16:20:33 +0100

    get and set explained. help method used

--- New-OOP-Practices/7-encapsulation.py
+++ New-OOP-Practices/7-encapsulation.py
@@ -5,11 +5,19 @@ class Personnel:
         self.last_name = last_name
         self.salary = salary
         self.email = f'{self.first_name.lower()}.{self.last_name.lower()}@firm.com'
-        self.__increase_rate = 1.1 #  With using '__' underscore to attribute we convert it into Private outside of the method it can't be reachable. 
+        self.__increase_rate = 1.1  #PRIVATE #  With using '__' underscore to attribute we convert it into Private outside of the method it can't be reachable. 
 
     def get_full_name(self):
         return f'{self.first_name} {self.last_name}'
     
+
+    def __get_increase_rate(self):
+        return self.__increase_rate 
+    
+    # def set_increase_rate(self, rate): 
+    #     self.__increase_rate = rate
+
+
     def increase_salary(self):
         self.salary = int(self.salary * self.__increase_rate) 
 
@@ -20,6 +28,15 @@ per2 = Personnel(salary = 5000, first_name = "Stephen", last_name = "Currry")
 # Without encapsulation we can assign change the rate. We do not want that.
 
 # per1.__increase_rate = 5
-print(per1.__dict__)
-per1.increase_salary()
-print(per1.salary)
+# print(per1.__dict__)
+# per1.increase_salary()
+# print(per1.salary)
+# # per1.set_increase_rate(2)
+# per1.increase_salary()
+# print(per1.get_increase_rate())
+# print(per1.salary)
+
+
+# PRIVATE METHOD
+# If we encapsulate get_increase_rate method we can not see it in help method. Why we want that because we may want to use our method inside of another method in our class.
+print(help(per1))
